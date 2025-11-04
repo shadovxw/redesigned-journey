403e3a6e0d3f9d603a499b5a18ad83bec1dfc8b1
an-odd-sheep <vrishank.w@somaiya.edu>
2025-11-04 22:47:01 +0530

    second commit

--- prepare_dataset.py
+++ prepare_dataset.py
@@ -1,6 +1,15 @@
 import kagglehub
+import shutil
+import os
 
-# Download latest version
+# Download dataset to cache (default behavior)
 path = kagglehub.dataset_download("paultimothymooney/chest-xray-pneumonia")
 
-print("Path to dataset files:", path)
+# Define destination (current directory)
+dest = os.path.join(os.getcwd(), "chest-xray-pneumonia")
+
+# Copy from cache to current dir if not already there
+if not os.path.exists(dest):
+    shutil.copytree(path, dest)
+
+print("Dataset copied to:", dest)
